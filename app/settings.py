c91481072e7aa4922a4a84c0387503f33ea8741f
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-09-26 07:45:08 +0500

    changesettings11111

--- app/settings.py
+++ app/settings.py
@@ -128,4 +128,3 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
