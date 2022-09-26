02aefc72458ccb5f14e4d38516f17a43be20377d
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-09-26 07:32:16 +0500

    changesettings

--- app/settings.py
+++ app/settings.py
@@ -127,6 +127,6 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (STATIC_DIR, )
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
 
